691ec7e0c5a6fe00cc8a2f43ba330f09f7c152dd
Gabriel Santos <gabrielbarretos2000@outlook.com>
2022-04-08 03:29:17 +0000

    some resolvers and services created

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -4,13 +4,28 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 import { DatabaseModule } from '../database/database.module';
-import { TestResolver } from './test.resolver';
+import { ProductsService } from '../services/products.service';
+import { PurchasesService } from '../services/purchases.service';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, GraphQLModule.forRoot({
-    autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
-    driver: ApolloDriver
-  })],
-  providers: [TestResolver]
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+  ],
+  providers: [
+    // resolvers
+    ProductsResolver,
+    PurchasesResolver,
+
+    // services
+    ProductsService,
+    PurchasesService,
+  ],
 })
 export class HttpModule {}
