5abd94cd8a910181984e266a10b2fbf1f4c476c5
Gabriel Santos <gabrielbarretos2000@outlook.com>
2022-04-20 20:10:49 +0000

    front-end finalized

---
+++ web/src/lib/withPublicApollo.tsx
@@ -0,0 +1,39 @@
+import { GetServerSidePropsContext, NextPage } from "next";
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  from,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client";
+
+export type ApolloClientContext = GetServerSidePropsContext;
+
+export const withPublicApollo = (Component: NextPage) => {
+  return function Provider(props: any) {
+    return (
+      <ApolloProvider client={getApolloClient(undefined, props.apolloState)}>
+        <Component {...props} />
+      </ApolloProvider>
+    );
+  };
+};
+
+export function getApolloClient(
+  ctx?: ApolloClientContext,
+  ssrCache?: NormalizedCacheObject
+) {
+  const httpLink = createHttpLink({
+    uri: "https://3332-gabrielbs21-ignitelab-i8f3blzlo63.ws-us34.gitpod.io/graphql",
+    fetch,
+  });
+
+  const cache = new InMemoryCache().restore(ssrCache ?? {});
+
+  return new ApolloClient({
+    link: from([httpLink]),
+    cache,
+  });
+}
