a2c59dabe76ad5922a0f44dc7d4e86c33e10fafe
Gabriel Santos <gabrielbarretos2000@outlook.com>
2022-04-20 06:08:36 +0000

    added graphql to frontend

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import { UserProvider } from '@auth0/nextjs-auth0'
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+import { UserProvider } from "@auth0/nextjs-auth0";
+
+import "../styles/global.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
